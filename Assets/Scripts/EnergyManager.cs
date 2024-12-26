b4c81c19a32c4a004fd8bdaf715a7952a115199b
GregCodi <74345615+GregCodi@users.noreply.github.com>
2024-12-26 11:16:00 +0000

    Deploying to gh-pages from @ GregCodi/ClickerTest@b4f7f3b7e556a4e60e0babd2be14fe8d67d8cbce 🚀

---
+++ Assets/Scripts/EnergyManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class EnergyManager : MonoBehaviour
+{
+    [SerializeField] private int maxEnergy = 100;
+    [SerializeField] private int currentEnergy;
+    [SerializeField] private float energyRegenRate = 1f;
+    [SerializeField] private TextMeshProUGUI energyText;
+    
+    private float regenTimer;
+
+    void Start()
+    {
+        currentEnergy = maxEnergy;
+    }
+
+    void Update()
+    {
+        regenTimer += Time.deltaTime;
+        if (regenTimer >= energyRegenRate && currentEnergy < maxEnergy)
+        {
+            currentEnergy++;
+            regenTimer = 0f;
+        }
+        
+        UpdateEnergyText(); 
+    }
+    
+    private void UpdateEnergyText()
+    {
+        if (energyText != null)
+        {
+            energyText.text = "Energy: " + currentEnergy + " / " + maxEnergy;
+        }
+    }
+    
+    public bool SpendEnergy(int amount)
+    {
+        if (currentEnergy >= amount)
+        {
+            currentEnergy -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.Log("Недостаточно энергии!"); 
+            return false;
+        }
+    }
+}
